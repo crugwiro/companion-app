22fb521e133c5afbdb9dcda2e3e89ed70db139d5
rowlinds <rowlinds@cisco.com>
2020-10-28 13:17:22 -0700

    quickstart script:

---
+++ bin/setup.sh
@@ -0,0 +1,8 @@
+# TODO check tools
+cd mobile/dependencies
+git clone -b dev-ts https://github.com/HeyMaslo/maslo-persona.git persona
+git clone https://github.com/HeyMaslo/react-native-switch-pro.git
+cd ../..
+yarn all
+cd mobile/ios
+pod install
